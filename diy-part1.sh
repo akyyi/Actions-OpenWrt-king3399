1256b60f7acb8c1afa994266fd832d786bc7324d
zh604131924 <50488791+zh604131924@users.noreply.github.com>
2022-11-21 11:04:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo ' src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
+echo ' src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
 git clone https://github.com/linkease/istore.git package/istore
 #sed -i 's/luci-lib-ipkg/luci-base/g' package/istore/luci/luci-app-store/Makefile
 git clone https://github.com/linkease/istore-ui.git package/istore-ui
